d6977a95ac18f2a6db07b2c157b25e7c2a8b7acb
Olamide Timothy Tawose <otawose@nevada.unr.edu>
2020-10-19 23:17:10 -0700

    Bash script for PyMP implementations iteratively

---
+++ equationbash.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# File: equationbash.sh
+# Author: otawose@nevada.unr.edu
+# Date: 2/4/2019
+# Desc: bash script to run PyMP Implementation on the Z820 server 
+#                      How to run scipt
+# chmod +x equationbash.sh
+# ./equationbash.sh 
+
+for ((i=1;i<=16;i*=2))   ### outer loop for threads up to 32 threads ###
+do
+	for ((j=10;j<=100;j+=10))   ### outer loop for array rank up to 100 ###
+	do
+		python eqs_pymp.py $j $i    ### change to python eqs_pympIO.py $j $i  in order to run Pymp implementation with I/O script
+	done
+done
