903f87a19f82fa18f9fe85a93244a79bb4ce184c
Olamide Timothy Tawose <otawose@nevada.unr.edu>
2021-04-09 22:05:11 -0700

    Update memory.sh

--- memory.sh
+++ memory.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 # File: memory.sh
-# Author: otawose@nevada.unr.edu
-# Date: 2/4/2019
+#     Date:   1/3/2021
 # Desc: bash script to observe the peak (i.e. the highest) memory usage and the time-based memory usage (at an interval of 0.1 seconds) for threads per specified rank in the various levels of parallelismin PyMP
 #                      How to run scipt
 # stary by installing the  python memory profiler module using "pip install memory-profiler"
